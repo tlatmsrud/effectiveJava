934367157593bb15c07668f3a9d23fa1eeb41c2e
tlatmsrud <tlatmsrud@naver.com>
2023-09-01 11:18:36 +0900

    :tada: Init-project

---
+++ effectivaJava/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * title        :
+ * author       : ${USER}
+ * date         : ${DATE}
+ * description  :
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
