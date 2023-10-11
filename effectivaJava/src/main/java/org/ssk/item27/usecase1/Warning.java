7161a889f0f7d8d33be370525377120e05a1f870
tlatmsrud <tlatmsrud@naver.com>
2023-10-11 16:55:42 +0900

    :sparkles: item-27-비검사-경고를-제거하라

---
+++ effectivaJava/src/main/java/org/ssk/item27/usecase1/Warning.java
@@ -0,0 +1,23 @@
+package org.ssk.item27.usecase1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-10-11
+ * description  :
+ */
+public class Warning {
+
+    public static void main(String[] args) {
+
+        Set<Coin> coinSet = new HashSet<>(); // 타입을 컴파일러가 추론하므로 HashSet 타입은 생략 가능하다.
+    }
+}
+
+class Coin{
+
+
+}
