7e9e87b2e3d39fffa07f05c5600ecd50973e17af
tlatmsrud <tlatmsrud@naver.com>
2023-11-14 05:13:57 +0900

    :sparkles: HashMap 이해하기

---
+++ effectivaJava/src/test/java/homework_20231113/HashMapTest.java
@@ -0,0 +1,25 @@
+package homework_20231113;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-11-13
+ * description  :
+ */
+public class HashMapTest {
+
+    @Test
+    void test(){
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        hashMap.put("Sim","010-1111-1111");
+        hashMap.put("Park","010-2222-2222");
+        hashMap.put("Hong","010-3333-3333");
+
+
+    }
+}
