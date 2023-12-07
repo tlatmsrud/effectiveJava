ec47e467ac13e51a2144f754f76fe68effc545bb
tlatmsrud <tlatmsrud@naver.com>
2023-12-07 23:16:22 +0900

    :sparkles: item-38

---
+++ effectivaJava/src/main/java/org/ssk/item38/usecase2/Main.java
@@ -0,0 +1,16 @@
+package org.ssk.item38.usecase2;
+
+import org.ssk.item20.usecase1.B;
+
+/**
+ * title        :
+ * author       : sim
+ * date         : 2023-12-06
+ * description  :
+ */
+public class Main {
+    public static void main(String[] args) {
+        AClass aClass = new BClass();
+        BClass bClass = new AClass();
+    }
+}
