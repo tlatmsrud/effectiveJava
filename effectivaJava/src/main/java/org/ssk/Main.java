f3d1c404febc40a494804a3c5d2862a52f9ac0f0
tlatmsrud <tlatmsrud@naver.com>
2024-01-25 02:13:18 +0900

    :art: BYE

--- effectivaJava/src/main/java/org/ssk/Main.java
+++ effectivaJava/src/main/java/org/ssk/Main.java
@@ -11,5 +11,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("BYE");
     }
 }
